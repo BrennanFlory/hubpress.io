cff8cfaa4b7997e4ebda7f1cd02fef4c73824d76
Luminaryfive <brennanjflory@gmail.com>
2018-01-26 15:21:25 -0500

    don

--- themes/saga/assets/js/scripts.js
+++ themes/saga/assets/js/scripts.js
@@ -7,8 +7,8 @@ siteurl += "/themes/saga";
  * HIGHLIGHT CODE *
  ******************/
 
-if($("code").length !== 0){
-    $.getScript(siteurl+"/assets/js/helper/highlight.min.js", function() {
+if ($("code").length !== 0) {
+    $.getScript(siteurl + "/assets/js/helper/highlight.min.js", function() {
         hljs.initHighlightingOnLoad();
     });
 }
@@ -17,7 +17,7 @@ if($("code").length !== 0){
  * RESPONSIVE VIDEOS  *
  **********************/
 
-$.getScript(siteurl+"/assets/js/helper/jquery.fitvids.js", function() {
+$.getScript(siteurl + "/assets/js/helper/jquery.fitvids.js", function() {
     $("#main").fitVids();
 });
 
@@ -25,8 +25,8 @@ $.getScript(siteurl+"/assets/js/helper/jquery.fitvids.js", function() {
  * GALLERY *
  ***********/
 
-if($('p a:not(:only-child) img').closest('p').length !== 0 || $('p img:not(:only-child)').closest('p').length !== 0){ // If there is a gallery present.
-    $.getScript(siteurl+"/assets/js/helper/imagesloaded.pkgd.min.js", function() {
+if ($('p a:not(:only-child) img').closest('p').length !== 0 || $('p img:not(:only-child)').closest('p').length !== 0) { // If there is a gallery present.
+    $.getScript(siteurl + "/assets/js/helper/imagesloaded.pkgd.min.js", function() {
         $('p a:not(:only-child) img').closest('p').addClass('gallery');
         $('p img:not(:only-child)').closest('p').addClass('gallery');
         $(".gallery").imagesLoaded(gallery);
@@ -34,22 +34,20 @@ if($('p a:not(:only-child) img').closest('p').length !== 0 || $('p img:not(:only
     });
 }
 
-function gallery(){
-    $.getScript(siteurl+"/assets/js/helper/gallery.min.js", function() { // Load in script for gallery
+function gallery() {
+    $.getScript(siteurl + "/assets/js/helper/gallery.min.js", function() { // Load in script for gallery
         var size = 0;
-        if ($(window).height() > $(window).width()){
+        if ($(window).height() > $(window).width()) {
             size = $(window).height();
         } else {
             size = $(window).width();
         }
-        if (size < 210 ){
+        if (size < 210) {
             size = 210;
         }
-        $('.gallery').removeWhitespace().collagePlus(
-            {
-                'targetHeight':size/5
-            }
-        );
+        $('.gallery').removeWhitespace().collagePlus({
+            'targetHeight': size / 5
+        });
     });
 }
 
@@ -57,12 +55,12 @@ function gallery(){
  * FULL WIDTH IMAGES *
  *********************/
 
-if($("#main").hasClass("content")){
-    $.getScript(siteurl+"/assets/js/helper/imagesloaded.pkgd.min.js", function() {
-        function fullImage(){
+if ($("#main").hasClass("content")) {
+    $.getScript(siteurl + "/assets/js/helper/imagesloaded.pkgd.min.js", function() {
+        function fullImage() {
             $('img[src$="#full"]:only-child').each(function() {
                 $(this).addClass("full-loaded");
-                $(this).closest("p").css("min-height",$(this).height());
+                $(this).closest("p").css("min-height", $(this).height());
                 $(this).closest("p").addClass("full-image-container");
             });
         }
@@ -75,17 +73,17 @@ if($("#main").hasClass("content")){
  * LIGHTBOX *
  ************/
 
-if($("#main").hasClass("content")){
-    $.getScript(siteurl+"/assets/js/helper/jquery.fluidbox.min.js", function() {
-        $.getScript(siteurl+"/assets/js/helper/imagesloaded.pkgd.min.js", function() {
-            function lightBox(){
+if ($("#main").hasClass("content")) {
+    $.getScript(siteurl + "/assets/js/helper/jquery.fluidbox.min.js", function() {
+        $.getScript(siteurl + "/assets/js/helper/imagesloaded.pkgd.min.js", function() {
+            function lightBox() {
                 $('.content a').filter(function() {
                     return $(this).attr('href').match(/\.(jpeg|jpg|png|gif)/i);
                 }).fluidbox({
-                        closeTrigger: [
-                            { selector: '#fluidbox-overlay', event: 'click'         },
-                            { selector: 'window',            event: 'resize scroll' }
-                        ]
+                    closeTrigger: [
+                        { selector: '#fluidbox-overlay', event: 'click' },
+                        { selector: 'window', event: 'resize scroll' }
+                    ]
                 });
             }
             $("#main").imagesLoaded(lightBox);
@@ -97,11 +95,11 @@ if($("#main").hasClass("content")){
  * STICKY FOOTER *
  *****************/
 
-$(window).load(function(){
-    $("#main").css("min-height",$(window).height() - $("#header").height() - $("#footer").height() );
+$(window).load(function() {
+    $("#main").css("min-height", $(window).height() - $("#header").height() - $("#footer").height());
 });
-$(window).resize(function(){
-    $("#main").css("min-height",$(window).height() - $("#header").height() - $("#footer").height() );
+$(window).resize(function() {
+    $("#main").css("min-height", $(window).height() - $("#header").height() - $("#footer").height());
 });
 
 /********
@@ -109,19 +107,19 @@ $(window).resize(function(){
  ********/
 
 var $masonry;
-if($("#main").hasClass("archive")){
-    $.getScript(siteurl+"/assets/js/helper/masonry.pkgd.min.js", function() {
-        $.getScript(siteurl+"/assets/js/helper/imagesloaded.pkgd.min.js", function() {
-            $("#main").imagesLoaded(function(){
+if ($("#main").hasClass("archive")) {
+    $.getScript(siteurl + "/assets/js/helper/masonry.pkgd.min.js", function() {
+        $.getScript(siteurl + "/assets/js/helper/imagesloaded.pkgd.min.js", function() {
+            $("#main").imagesLoaded(function() {
                 $masonry = $('.feed').masonry({
                     columnWidth: '.post:not(.featured)',
                     itemSelector: '.post',
                     gutter: 20
                 });
-                $('.post').each(function(){
+                $('.post').each(function() {
                     $(this).css("opacity", "1.0");
                 });
-                $('#loadmore').each(function(){
+                $('#loadmore').each(function() {
                     $(this).css("opacity", "1.0");
                 });
             });
@@ -133,7 +131,7 @@ if($("#main").hasClass("archive")){
  * LOAD MORE POSTS *
  *******************/
 
-if($("#main").hasClass("archive")){
+if ($("#main").hasClass("archive")) {
     $(document).ready(function($) {
 
         // The number of the next page to load (/page/x/).
@@ -147,7 +145,7 @@ if($("#main").hasClass("archive")){
          * Replace the traditional navigation with our own,
          * but only if there is at least one page of new posts to load.
          */
-        if(pageNum < max) {
+        if (pageNum < max) {
             // Insert the "More Posts" link.
             $('#feed').append('<div id="loadmore" style="opacity: 0;"><a class="btn">Load more <i class="fa fa-plus-circle"></i></a></div>');
 
@@ -163,27 +161,27 @@ if($("#main").hasClass("archive")){
          */
         $('#loadmore a').click(function() {
             // Are there more posts to load?
-            if(pageNum < max) {
+            if (pageNum < max) {
 
                 // Show that we're working.
                 $(this).html('<i class="fa fa-spinner fa-spin"></i>');
 
                 // Grab data from next page
-                $.get(nextLink, function(data){
+                $.get(nextLink, function(data) {
                     // Append all posts to #content
                     var posts = $(data).find(".post");
-                    $.each(posts,function(){
+                    $.each(posts, function() {
                         $(this).css("opacity", 0);
                     });
                     $masonry.append(posts);
                     // Change nextLink to next page
-                    $("#feed").imagesLoaded(function(){
+                    $("#feed").imagesLoaded(function() {
                         pageNum++;
                         nextLink = nextLink.substring(0, nextLink.indexOf('page/'));
-                        nextLink += "page/"+(pageNum+1);
+                        nextLink += "page/" + (pageNum + 1);
 
                         // Remove button if last page else move the button to end of #content
-                        if(pageNum < max) {
+                        if (pageNum < max) {
                             $('#loadmore').insertAfter($('#feed .post:last'));
                             $('#loadmore a').html('Load more <i class="fa fa-plus-circle"></i>');
                         } else {
